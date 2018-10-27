6bc4fe9c4a7ef57fab0456752cca35fb97edab87
Marc Slakey <marcslakey@gmail.com>
2018-10-27 13:01:23 -0700

    Separated Weapon class into RangedWeapon and MeleeWeapon classes

--- weapon.py
+++ weapon.py
@@ -2,12 +2,8 @@
 
 class Weapon(object):
 	"""docstring for Weapon"""
-	def __init__(self, name, w_range, w_type, shot_dice, shots, strength, ap, damage_dice, damage, hit_function=None, wound_function=None, save_function=None):
+	def __init__(self, name, strength, ap, damage_dice, damage, hit_function=None, wound_function=None, save_function=None):
 		self.name = name
-		self.w_range = w_range
-		self.w_type = w_type
-		self.shot_dice = shot_dice
-		self.shots = shots
 		self.strength = strength
 		self.ap = ap
 		self.damage_dice = damage_dice
@@ -15,3 +11,20 @@ class Weapon(object):
 		self.hit_function = hit_function
 		self.wound_function = wound_function
 		self.save_function = save_function
+
+class RangedWeapon(Weapon):
+	"""docstring for RangedWeapon"""
+	def __init__(self, name, w_range, w_type, shot_dice, shots, strength, ap, damage_dice, damage, hit_function=None, wound_function=None, save_function=None):
+		#Run the __init__ function on yourself of the super/parent of the RangedWeapon class (which is the Weapon class)
+		super(RangedWeapon, self).__init__(name, strength, ap, damage_dice, damage, hit_function, wound_function, save_function)
+		self.w_range = w_range
+		self.w_type = w_type
+		self.shot_dice = shot_dice
+		self.shots = shots
+
+class MeleeWeapon(Weapon):
+	"""docstring for MeleeWeapon"""
+	def __init__(self, name, strength, ap, damage_dice, damage, hit_function=None, wound_function=None, save_function=None):
+		#Run the __init__ function on yourself of the super/parent of the MeleeWeapon class (which is the Weapon class)
+		super(MeleeWeapon, self).__init__(name, strength, ap, damage_dice, damage, hit_function, wound_function, save_function)
+		
