1fd6adba34e51e27ff6e564717b4afab990c8d9d
Marc Slakey <marcslakey@gmail.com>
2018-10-27 09:11:47 -0700

    Finished initiazling unit class

--- unit.py
+++ unit.py
@@ -1,8 +1,15 @@
-
-
 class Unit(object):
 	"""docstring for Unit"""
-	def __init__(self, hp):
+	def __init__(self, name, move, weapon_skill, ballistic_skill, strength, toughness, wounds, attacks, leadership, save, invulnerable):
 		super(Unit, self).__init__()
-		self.hp = hp
-		
+		self.name = name
+		self.move = move
+		self.weapon_skill = weapon_skill
+		self.ballistic_skill = ballistic_skill
+		self.strength = strength
+		self.toughness = toughness
+		self.wounds = wounds
+		self.attacks = attacks
+		self.leadership = leadership
+		self.save = save
+		self.invulnerable = invulnerable
