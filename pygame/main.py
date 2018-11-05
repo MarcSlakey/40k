5bf28add743e5740a565ad9f546adcb1157455de
Marc Slakey <marcslakey@gmail.com>
2018-11-05 14:49:57 -0800

    Modified comments, added a print statement to reset_moves()

--- pygame/main.py
+++ pygame/main.py
@@ -1,12 +1,12 @@
 """40k Pygame
 
-Three Sprites are spawned, two named model1 and model2, and the other named target.
+Four Sprites are spawned, three controllable models named model1, model2, and model3, and one non-controllable model named target.
 
-Left clicking a sprite selects it, left clicking empty space deselects it. 
+Left clicking a controllable model selects it, left clicking empty space deselects it. 
 	Selecting a model causes three circles to be drawn: yellow for move distance, red for weapon range, and green to highlight the selected sprite.
-Right click moves the model to the clicked location if it within the remaining move distance.
+Right click moves the selected model to the clicked location if it is within the model's remaining move distance.
 Space resets the selected model to its original position and restores its max move distance.
-Middle click will attempt to delete the target. If the model selected is within shooting range of target, the target will be killed.
+Middle click while hovering the mouse over the target model will delete it if the currently selected model is within shooting range.
 """
 
 import pygame
@@ -16,8 +16,8 @@ from sprites import *
 
 
 class Game:
+	#Initialize program, game window, etc.
 	def __init__(self):
-		#Initialize program, game window, etc.
 		pygame.init()				#Always needed 
 		pygame.mixer.init()			#Always needed if you want any sound
 		self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -27,7 +27,6 @@ class Game:
 
 	#Initialize a new game
 	def new(self):
-		
 		self.all_sprites = pygame.sprite.Group() 	#All-inclusive group of sprites, simplifies updating and drawing
 		self.model_sprites = pygame.sprite.Group()
 
@@ -48,7 +47,7 @@ class Game:
 		self.playing = True
 		while self.playing:
 			self.clock.tick(FPS)	#Update the clock. Should be called once per "frame" (game loop?)
-			self.events()		#Meat of the program
+			self.events()				#Meat of the program
 			self.update()
 			self.draw()
 
@@ -57,40 +56,46 @@ class Game:
 		keys = pygame.key.get_pressed()
 		if self.selected_model != None and keys[pygame.K_SPACE]:
 			if self.selected_model.x != self.selected_model.original_pos[0] and self.selected_model.y != self.selected_model.original_pos[1]:
-				print("\nSprite at ({},{}) reset to original_pos = ({},{})".format(self.selected_model.x, self.selected_model.y, 
-																				self.selected_model.original_pos[0], self .selected_model.original_pos[1]))
 				self.selected_model.x = self.selected_model.original_pos[0]
 				self.selected_model.y = self.selected_model.original_pos[1]
 				self.selected_model.max_move = self.selected_model.original_max_move
+				print("\nSprite at ({},{}) reset to original_pos = ({},{})".format(self.selected_model.x, self.selected_model.y, 
+																				self.selected_model.original_pos[0], self .selected_model.original_pos[1]))
+				print("\nMax move reset to {}".format(selected_model.original_max_move))
 
+	#Game Loop - Event Handling
 	def events(self):
-		#Game Loop - Event Handling
 		for event in pygame.event.get():	#event.get() returns a list of all events currently in the queue and also removes all these events from the queue
 			if event.type == pygame.QUIT:
 				if self.playing:
 					self.playing = False
 				self.running = False
 
+			#Keyboard event handling
 			elif event.type == pygame.KEYDOWN:
 				self.reset_moves()
 
-
+			#Mouse event handling
 			elif event.type == pygame.MOUSEBUTTONUP:
+				#If a model is not selected, LMB selects a model.
 				if self.selected_model == None:
-					if event.button == 1:		#mouse event.buttom refers to interger values: 1(left), 2(middle), 3(right), 4(scrl up), 5(scrl down)
+					if event.button == 1:	#Mouse event.buttom refers to interger values: 1(left), 2(middle), 3(right), 4(scrl up), 5(scrl down)
 						for self.model in self.model_sprites:
 							if self.model.rect.collidepoint(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]):
 								self.selected_model = self.model
 
+				#If a model is selected, LMB deselects it, RMB moves it, and Middle mouse button shoots.
 				elif self.selected_model != None:
-					if event.button == 1:	
+					if event.button == 1:	#LMB
 						for self.model in self.model_sprites:
+							#This line doesn't work (not sure why); should allow selecting a different sprite while one is already selected
 							if self.model.rect.collidepoint(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]):
 								self.selected_model = self.model
-							else:	#commenting these 2 lines out allows selection of new sprite while one is selected; ideally want that as well as deselection functionality
+							#Commenting this else out allows selection of new sprite while one is selected, but removes deselection
+							else:	
 								self.selected_model = None
 
-					elif event.button == 2:
+					elif event.button == 2:	#Middle mouse button
 						shot_x = self.selected_model.x - pygame.mouse.get_pos()[0]
 						shot_y = self.selected_model.y - pygame.mouse.get_pos()[1]
 						shot_distance = find_hypotenuse(shot_x, shot_y)
@@ -98,13 +103,12 @@ class Game:
 							if self.target.rect.collidepoint(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]):		#Returns true if the spot clicked is in the target's rect
 								self.target.kill()
 
-					elif event.button == 3:
+					elif event.button == 3: #RMB
 						self.selected_model.dest_x = pygame.mouse.get_pos()[0]
 						self.selected_model.dest_y = pygame.mouse.get_pos()[1]
 				
-
+	#Game Loop - Update
 	def update(self):
-		#Game Loop - Update
 		self.all_sprites.update()
 
 	def draw_grid(self):
@@ -113,14 +117,14 @@ class Game:
 		for y in range(0, HEIGHT, TILESIZE):		#draws horizontal lines
 			pygame.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
 
+	#Draws various useful circles: max move, weapon range, and a selection indicator
 	def draw_radii(self):
 		pygame.draw.circle(self.screen, YELLOW, (self.selected_model.x, self.selected_model.y), self.selected_model.max_move, 1)		#Draw surface, color, location, radius, width
 		pygame.draw.circle(self.screen, RED, (self.selected_model.x, self.selected_model.y), self.selected_model.weapon_range, 1)
 		pygame.draw.circle(self.screen, GREEN, (self.selected_model.x, self.selected_model.y), 32, 1)
 
-
+	#Game Loop - Draw
 	def draw(self):
-		#Game Loop - Draw
 		self.screen.fill(BLACK)
 		self.draw_grid()
 		if self.selected_model != None:
